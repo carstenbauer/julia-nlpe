75b4c534d39e07bce295ef08dbe8e8d7d8693f86
Carsten Bauer <crstnbr@gmail.com>
2021-10-01 18:01:40 +0200

    flops_dp + data

--- julia/stream-mapi/stream-mapi.sh
+++ julia/stream-mapi/stream-mapi.sh
@@ -1,3 +1,6 @@
 # likwid-perfctr -s 0xfffffffffffffe01 -C 0-7 -m -g FLOPS_DP julia --project=. -t 8 stream-mapi.jl
 # likwid-perfctr -c 0-7 -m -g FLOPS_DP likwid-pin -s 0xfffffffffffffe01 -c 0-7 julia --project=. -t 8 stream-mapi.jl
+echo "##################### FLOPS_DP #####################"
 JULIA_EXCLUSIVE=1 likwid-perfctr -c 0-7 -m -g FLOPS_DP julia --project=. -t 8 stream-mapi.jl
+echo "##################### DATA #####################"
+JULIA_EXCLUSIVE=1 likwid-perfctr -c 0-7 -m -g DATA julia --project=. -t 8 stream-mapi.jl
